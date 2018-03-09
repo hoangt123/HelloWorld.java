89770ff948b64d60f83d8208b80fd15d50246f03
Hoang <hoangtran@berkeley.edu>
2018-03-08 23:10:09 -0800

    Practice
    Practice.

---
+++ LeapYear.java
@@ -0,0 +1,15 @@
+public class LeapYear {
+	public static void LeapYear(int year) {
+		if (year % 100 == 0 || year % 4 == 0 || year % 400 == 0) {
+		System.out.println (year + " is a leap year.");
+		}
+		else {
+		System.out.println (year + " is not a leap year.");
+		}
+	}
+
+    public static void main(String[] args) {
+        LeapYear(2000);
+        
+    }
+}
